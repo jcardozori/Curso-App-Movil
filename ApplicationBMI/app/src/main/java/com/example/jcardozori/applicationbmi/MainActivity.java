5d8770d278564a6f37f17090a36f253392586f5d
jcardozori <jcardozori@gmail.com>
2017-01-25 18:34:09 -0500

    Evento OnClick

--- ApplicationBMI/app/src/main/java/com/example/jcardozori/applicationbmi/MainActivity.java
+++ ApplicationBMI/app/src/main/java/com/example/jcardozori/applicationbmi/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.jcardozori.applicationbmi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private EditText editTextAltura;
+    private EditText editTextPeso;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        EditText editText = (EditText) findViewById(R.id.editTextAltura);
+        editTextAltura = (EditText) findViewById(R.id.editTextAltura);
+        editTextPeso = (EditText) findViewById(R.id.editTextPeso);
+    }
+
+    public void CalculaBMI(View view) {
+        String peso = editTextPeso.getText().toString();
+        String altura = editTextAltura.getText().toString();
+        if (peso.isEmpty() || altura.isEmpty()){
+            Log.e(TAG,"Campos peso y altura estan vacios");
+            return;
+        }
+        String expRegex = "\\d{1,3}";
+        if (peso.matches(expRegex) && altura.matches(expRegex))
+        {
+            Log.d(TAG,"No son numeros");
+        }
+
+
     }
 }
